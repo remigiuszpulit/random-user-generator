bc63291b2185d6b181a5add761c812cdd20b51e2
remigiuszpulit <remigiusz.pulit@gmail.com>
2021-12-27 21:47:07 +0100

    init

---
+++ .history/random_usr_gen/app_20211227213611.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
+  methods: {
+    getUser() {
+      (this.firstName = "dupa"),
+        (this.lastName = "dupa"),
+        (this.email = "john@gmail.com"),
+        (this.gender = "female"),
+        (this.picture = "https://randomuser.me/api/portraits/men/10.jpg");
+    },
+  },
+});
+
+app.mount("#app");
---
+++ .history/random_usr_gen/app_20211227213919.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json;
+      console.log(results)((this.firstName = "dupa")),
+        (this.lastName = "dupa"),
+        (this.email = "john@gmail.com"),
+        (this.gender = "female"),
+        (this.picture = "https://randomuser.me/api/portraits/women/10.jpg");
+    },
+  },
+});
+
+app.mount("#app");
